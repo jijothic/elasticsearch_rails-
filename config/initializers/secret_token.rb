9abc4fc192670fa855357505859092720e02fe08
jijothic <geogtz@yahoo.com>
2016-11-26 16:00:44 +0300

    Added search and elasticsearch gem

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ElasticsearchRails::Application.config.secret_token = '1e899a14cecee3930dc948948aa5ec3e9761aedd5684bfbf40e4c7f1bf6688e42faa7b7491783f5ec6cc14fe1227570d751507ef6e0c7cda62113ee45277f910'
